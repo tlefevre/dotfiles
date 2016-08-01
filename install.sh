1bd5da9653d4bbd8a5286fff78227846b8851d10
Brian Johnsen <info@brianjohnsen.dk>
2016-08-01 12:20:19 +0200

    Removed zsh relevant

--- install.sh
+++ install.sh
@@ -14,23 +14,6 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
-# Make ZSH the default shell environment
-chsh -s $(which zsh)
-
-# Install Composer
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
-
-# Install global Composer packages
-/usr/local/bin/composer global require laravel/installer laravel/lumen-installer laravel/valet tightenco/jigsaw
-
-# Install Laravel Valet
-valet install
-
-# Create a Sites directory
-# This is a default directory for OS X user accounts but doesn't comes pre-installed
-mkdir $HOME/Sites
-
 # Set OS X preferences
 # We will run this last because this will reload the shell
 source .osx
