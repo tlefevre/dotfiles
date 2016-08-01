75003cfd1e67a2c5994b11907e01505644694850
Brian Johnsen <info@brianjohnsen.dk>
2016-08-01 17:34:09 +0200

    Added sdkman and bash-it to install.sh

--- install.sh
+++ install.sh
@@ -14,6 +14,14 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
+# SDKMAN
+curl -s "https://get.sdkman.io" | bash
+
+# Bash-it
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh
+
+
 # Set OS X preferences
 # We will run this last because this will reload the shell
 source .osx
