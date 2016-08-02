4db68bc032afff190476a2656863fea9571bbd61
Brian Johnsen <info@brianjohnsen.dk>
2016-08-02 08:53:33 +0200

    Adds custom env and aliases for bash-it

---
+++ bash-it-custom/aliases/custom.aliases.bash
@@ -0,0 +1,7 @@
+## Gradle
+alias gwcc='./gradlew clean check'
+alias gwcodenarc='./gradlew codenarcMain codenarcTest codenarcIntegrationTest'
+alias gw='./gradlew'
+
+## SKAT
+alias skattunnel='ssh -N -L 127.0.0.1:7011:sktdemo01esb01:7011 -L 127.0.0.1:7013:sktdemo01esb01:7013 admin@sktdemo01jmp01.ccta.dk'
---
+++ bash-it-custom/lib/custom.bash
@@ -0,0 +1,3 @@
+export GRAILS_OPTS="-Xmx2G -Xms512m -XX:MaxPermSize=512m -Dfile.encoding=UTF-8 -Djava.encoding=UTF-8"
+
+export LC_ALL=da_DK.UTF-8
