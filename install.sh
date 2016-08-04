b49e6664d3e8ae26d891b6c949c8352647efa3aa
Brian Johnsen <info@brianjohnsen.dk>
2016-08-04 08:35:16 +0200

    Updates installer
    - Checks if sdkman and bash-it is present before installing
    - Copies custom alias and scripts to bash-it
    - Sources shell

--- install.sh
+++ install.sh
@@ -10,17 +10,36 @@ fi
 # Update Homebrew recipes
 brew update
 
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
 brew bundle
 
-# SDKMAN
-curl -s "https://get.sdkman.io" | bash
+# Remove outdated versions from the cellar.
+brew cleanup
 
-# Bash-it
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-~/.bash_it/install.sh --silent
+# Check for SDKMAN and install if not present
+if [[ ! $SDKMAN_VERSION ]]; then
+    curl -s "https://get.sdkman.io" | bash
+fi
 
+# Check for Bash-It and install if not present
+if [[ ! $BASH_IT ]]; then
+    git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+    ~/.bash_it/install.sh --silent
+fi
+
+# Copy custom stuff to .bash_it
+cp -v ~/.dotfiles/bash-it-custom/aliases/custom.aliases.bash ~/.bash_it/aliases/
+cp ~/.dotfiles/bash-it-custom/lib/custom.bash ~/.bash_it/lib/
+
+# Update Bash-it
+bash-it update
+
+# Reload
+source ~/.bash_profile
 
 # Set OS X preferences
 # We will run this last because this will reload the shell
