6f3378142cfd49f490fb3c7b028215e6499ad0a5
Brian Johnsen <info@brianjohnsen.dk>
2016-08-10 09:15:11 +0200

    Added to settings and custom scripts/aliases

--- bash-it-custom/aliases/custom.aliases.bash
+++ bash-it-custom/aliases/custom.aliases.bash
@@ -10,6 +10,7 @@ alias push="git push"
 
 ## SKAT
 alias skattunnel='ssh -N -L 127.0.0.1:7011:sktdemo01esb01:7011 -L 127.0.0.1:7013:sktdemo01esb01:7013 admin@sktdemo01jmp01.ccta.dk'
+alias skatdualnetwork='ccta'
 
 
 # Lock screen wallpaper
--- bash-it-custom/lib/custom.bash
+++ bash-it-custom/lib/custom.bash
@@ -1,3 +1,16 @@
 export GRAILS_OPTS="-Xmx2G -Xms512m -XX:MaxPermSize=512m -Dfile.encoding=UTF-8 -Djava.encoding=UTF-8"
 
 export LC_ALL=da_DK.UTF-8
+
+
+## Enables dual network at SKAT (WiFi must be at the top!)
+function ccta() {
+    sudo route -n delete 192.168.146.0 10.3.0.1 -netmask 255.255.255.0
+    sudo route -n delete 172.20.0.0 10.3.0.1 -netmask 255.255.0.0
+    sudo route -n delete 172.23.0.0 10.3.0.1 -netmask 255.255.0.0
+    sudo route -n delete 172.24.0.0 10.3.0.1 -netmask 255.255.0.0
+    sudo route -n add 192.168.146.0 10.3.0.1 -netmask 255.255.255.0
+    sudo route -n add 172.20.0.0 10.3.0.1 -netmask 255.255.0.0
+    sudo route -n add 172.23.0.0 10.3.0.1 -netmask 255.255.0.0
+    sudo route -n add 172.24.0.0 10.3.0.1 -netmask 255.255.0.0
+}
