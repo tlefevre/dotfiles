a586a477ce6bc44d751649b8a7349a51ad95d0c7
Brian Johnsen <info@brianjohnsen.dk>
2017-01-14 21:02:17 +0100

    Enables bash-it stuff

---
+++ bash-it.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Setting up bash-it..."
+
+echo "Setting up aliases"
+bash-it enable alias general
+
+echo "Setting up plugins"
+bash-it enable plugin alias-completion fasd git history sdkman
+
+echo "Setting up completions"
+bash-it enable completion bash-it git gradle sdkman system vagrant
