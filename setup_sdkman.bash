039e4acc6d2bb92792a1bcdbb5f3c8ed0c7eadd6
Brian Johnsen <info@brianjohnsen.dk>
2017-01-23 17:03:56 +0100

    Adds silent sdkman installs

--- setup_sdkman.bash
+++ setup_sdkman.bash
@@ -4,8 +4,8 @@ source ~/.bashrc
 
 echo "Setting up sdkman..."
 
-sdk install grails
-sdk install grails 2.5.0  < /dev/null #Quiet mode (https://github.com/sdkman/sdkman-cli/issues/101)
-sdk install groovy
-sdk install gradle
-sdk install maven
+yes | sdk install grails
+yes | sdk install grails 2.5.0
+yes | sdk install groovy
+yes | sdk install gradle
+yes | sdk install maven
