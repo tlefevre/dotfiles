14cddf973833c2522dfd21c71831911a67578878
Brian Johnsen <info@brianjohnsen.dk>
2017-01-25 09:20:20 +0100

    Adds virtualbox and vagrant

--- setup_apt-get.bash
+++ setup_apt-get.bash
@@ -7,7 +7,7 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 
 echo "Installing..."
-#Chrome
+# Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
@@ -17,7 +17,7 @@ sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install -y sublime-text-installer
 
-#Java
+# Java
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install -y oracle-java7-installer
@@ -51,16 +51,22 @@ sudo apt-get install -y duplicity
 # Preview (http://www.omgubuntu.co.uk/2016/09/gnome-sushi-mac-quick-look-nautilus)
 sudo apt-get install -y gnome-sushi
 
-#Ubuntu make
+# Ubuntu make
 sudo apt-get install -y ubuntu-make
 sudo umake ide idea-ultimate
 
-#MySQL
+# MySQL
 if [ -f ! /etc/init.d/mysql* ]; then
-    sudo apt-get install mysql-server
+    sudo apt-get install -y mysql-server
     # only run init script first time
     sudo mysql_secure_installation
 fi
+
+# VirtualBox
+sudo apt install -y virtualbox-qt virtualbox-ext-pack
+# Vagrant
+sudo apt install vagrant
+
 
 
 echo "Done installing!"
