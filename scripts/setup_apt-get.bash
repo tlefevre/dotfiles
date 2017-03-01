1abe217e391af4884da7c73ab90f8f568c0e73ca
Thomas Lefèvre <tle@nine.dk>
2017-03-01 14:52:29 +0100

    Added correct docker install

--- scripts/setup_apt-get.bash
+++ scripts/setup_apt-get.bash
@@ -51,8 +51,22 @@ sudo apt-get install -y wkhtmltopdf
 
 sudo apt-get install -y vim
 
-# Insert docker install
-#sudo apt install -y docker.io
+# Docker
+sudo apt-get install -y --no-install-recommends \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
+
+sudo apt-get update
+sudo apt-get -y install docker-engine
 
 sudo apt-get install -y bleachbit
 
